a55684709060bd4dab14607f3a8fe62e1a165be2
kala-alt <dimitrov3000@abv.bg>
2023-03-29 22:57:55 +0300

    C# TestOk

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
-using System.IO;  // include the System.IO namespace
+using System.IO;
 
 namespace MyApplication
 {
+
+    class Car {
+        string model = "Ford";
+        string color = "red";
+        Car() { 
+        
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            string a = Convert.ToString(x);
+            double f = 5.6;
+            int y = (int) Math.Round(f);
             Console.WriteLine("Nice!");
+            Console.WriteLine(y);
         }
     }
 }
