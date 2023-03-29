016b3cb30158894b0a139786c0206ecc6aef682f
kala-alt <dimitrov3000@abv.bg>
2023-03-29 22:43:01 +0300

    C# Test

---
+++ TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;  // include the System.IO namespace
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nice!");
+        }
+    }
+}
